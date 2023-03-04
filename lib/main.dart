904a5bfc700f9cec462531473bff53b51d7628c8
oculosdanilo <danilo.lima124@etec.sp.gov.br>
2023-03-04 16:58:57 -0300

    fdgdfg

--- lib/main.dart
+++ lib/main.dart
@@ -154,7 +154,7 @@ class LoginState extends State<FormApp> {
                     // If the form is valid, display a snackbar. In the real world,
                     // you'd often call a server or save the information in a database.
                     ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing Data')),
+                      const SnackBar(content: Text('hmmm')),
                     );
                   }
                 },
