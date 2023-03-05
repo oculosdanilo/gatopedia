3f4ef24cdc960df92896bc6ba264348cece4b3a3
oculosdanilo <danilo.lima124@etec.sp.gov.br>
2023-03-04 23:19:29 -0300

    dfdsafdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_typing_uninitialized_variables, prefer_const_literals_to_create_immutables, prefer_const_constructors, duplicate_ignore
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 // ignore: unnecessary_import
 import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _urlFlutter = Uri.parse('https://flutter.dev');
+final Uri _urlMaterialYou = Uri.parse('https://m3.material.io');
 
 void main() {
   final blueScheme = ColorScheme.fromSeed(
-      seedColor: Color(0xff000080), brightness: Brightness.dark);
+      seedColor: const Color(0xff000080), brightness: Brightness.dark);
   runApp(MaterialApp(
     theme: ThemeData(
       inputDecorationTheme: InputDecorationTheme(
@@ -15,7 +20,7 @@ void main() {
           borderSide: BorderSide(width: 2, color: blueScheme.outline),
         ),
         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(50)),
+          borderRadius: const BorderRadius.all(Radius.circular(50)),
           borderSide: BorderSide(width: 2, color: blueScheme.primary),
         ),
         errorBorder: OutlineInputBorder(
@@ -28,7 +33,7 @@ void main() {
         ),
       ),
       brightness: Brightness.dark,
-      colorSchemeSeed: Color(0xff000080),
+      colorSchemeSeed: const Color(0xff000080),
       useMaterial3: true,
     ),
     home: Gatopedia(),
@@ -36,7 +41,9 @@ void main() {
 }
 
 class Gatopedia extends StatelessWidget {
-  const Gatopedia({super.key});
+  Gatopedia({super.key});
+  final blueScheme = ColorScheme.fromSeed(
+      seedColor: const Color(0xff000080), brightness: Brightness.dark);
 
   @override
   Widget build(BuildContext context) {
@@ -45,10 +52,9 @@ class Gatopedia extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            // ignore: prefer_const_literals_to_create_immutables
             children: [
-              Image(
-                image: const AssetImage('lib/assets/icon.png'),
+              const Image(
+                image: AssetImage('lib/assets/icon.png'),
                 width: 270,
               ),
               Text(
@@ -58,10 +64,10 @@ class Gatopedia extends StatelessWidget {
                     fontFamily: "Jost",
                     fontWeight: FontWeight.bold),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 60,
               ),
-              SizedBox(
+              const SizedBox(
                 width: 300,
               ),
               const FormApp()
@@ -84,6 +90,15 @@ class FormApp extends StatefulWidget {
 
 class LoginState extends State<FormApp> {
   final _formKey = GlobalKey<FormState>();
+  final blueScheme = ColorScheme.fromSeed(
+      seedColor: const Color(0xff000080), brightness: Brightness.dark);
+
+  int txtFieldLenght = 0;
+  dynamic counterColor;
+
+  mudarCor(cor) {
+    counterColor = cor;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,6 +110,14 @@ class LoginState extends State<FormApp> {
             width: 300,
             child: TextFormField(
               onChanged: (value) {
+                setState(() {
+                  txtFieldLenght = value.length;
+                  if (value.length <= 4 || value.length > 25) {
+                    mudarCor(blueScheme.error);
+                  } else {
+                    mudarCor(blueScheme.primary);
+                  }
+                });
                 if (_formKey.currentState!.validate()) {
                   // If the form is valid, display a snackbar. In the real world,
                   // you'd often call a server or save the information in a database.
@@ -103,48 +126,76 @@ class LoginState extends State<FormApp> {
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Obrigatório';
-                } else if (!value.contains(RegExp(r'^[a-zA-Z0-9]+$'))) {
+                } else if (!value.contains(RegExp(r'^[a-zA-Z0-9._]+$'))) {
                   return 'Caractere(s) inválido(s)!';
+                } else if (value.length <= 4) {
+                  return "Nome muito pequeno!";
+                } else if (value.contains(RegExp(r'^[0-9]+$'))) {
+                  return "só números? sério?";
+                } else if (value.length > 25) {
+                  return "Nome de usuário muito grande!";
                 }
                 return null;
               },
               decoration: InputDecoration(
-                  prefixIcon: Icon(
+                  prefixIconColor: MaterialStateColor.resolveWith(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.error)) {
+                      return blueScheme.error;
+                    }
+                    if (states.contains(MaterialState.focused)) {
+                      return blueScheme.primary;
+                    }
+                    return blueScheme.outline;
+                  }),
+                  counter: SizedBox(
+                    width: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("$txtFieldLenght",
+                            style: TextStyle(color: counterColor)),
+                        Text(
+                          "/25",
+                          style: TextStyle(color: blueScheme.outline),
+                        )
+                      ],
+                    ),
+                  ),
+                  prefixIcon: const Icon(
                     Icons.alternate_email_rounded,
                   ),
-                  label: Text("Login", style: TextStyle(fontFamily: "Jost"))),
+                  label: const Text("Login",
+                      style: TextStyle(fontFamily: "Jost"))),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           SizedBox(
             width: 300,
             child: TextFormField(
               obscureText: true,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                   label: Text("Senha", style: TextStyle(fontFamily: "Jost"))),
             ),
           ),
-          SizedBox(
-            height: 25,
+          const SizedBox(
+            height: 27,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
                 onPressed: () {
-                  // SystemNavigator.pop();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('saindo...')),
-                  );
+                  SystemNavigator.pop();
                 },
-                child: Text(
+                child: const Text(
                   "Sair",
                   style: TextStyle(fontSize: 18),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 15,
               ),
               FilledButton(
@@ -158,47 +209,189 @@ class LoginState extends State<FormApp> {
                     );
                   }
                 },
-                child: Text(
+                child: const Text(
                   "Cadastrar",
                   style: TextStyle(fontSize: 18),
                 ),
               ),
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 5,
           ),
           OutlinedButton(
               onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Colaboradores()));
+                Navigator.push(context, SlideRightRoute(const Colaboradores()));
               },
-              child: Text("COLABORADORES")),
+              child: const Text("COLABORADORES")),
         ],
       ),
     );
   }
 }
 
-class Colaboradores extends StatelessWidget {
-  Colaboradores({super.key});
+class SlideRightRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideRightRoute(this.page)
+      : super(
+          reverseTransitionDuration: const Duration(milliseconds: 500),
+          transitionDuration: const Duration(milliseconds: 800),
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(0, 1),
+              end: Offset.zero,
+            ).animate(CurvedAnimation(
+              parent: animation,
+              curve: Curves.fastOutSlowIn,
+              reverseCurve: Curves.fastOutSlowIn,
+            )),
+            child: child,
+          ),
+        );
+}
+
+class Colaboradores extends StatefulWidget {
+  const Colaboradores({super.key});
+
+  @override
+  ColaboradoresState createState() {
+    return ColaboradoresState();
+  }
+}
+
+enum MenuItens { itemUm }
+
+class ColaboradoresState extends State {
   final blueScheme = ColorScheme.fromSeed(
-      seedColor: Color(0xff000080), brightness: Brightness.dark);
+      seedColor: const Color(0xff000080), brightness: Brightness.dark);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // ignore: prefer_const_literals_to_create_immutables
-      body: CustomScrollView(slivers: [
+      body: CustomScrollView(slivers: <Widget>[
         SliverAppBar.large(
           iconTheme: IconThemeData(color: blueScheme.onPrimary),
           title: Text(
             "COLABORADORES",
-            style: TextStyle(color: blueScheme.onPrimary),
+            style: TextStyle(color: blueScheme.onPrimary, fontFamily: "Jost"),
           ),
+          actions: [
+            PopupMenuButton<MenuItens>(
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<MenuItens>>[
+                PopupMenuItem(
+                  onTap: () {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.info_rounded,
+                                    color: blueScheme.onBackground,
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    "Sobre o projeto",
+                                    style: TextStyle(
+                                        fontFamily: "Jost",
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                              content: RichText(
+                                text: TextSpan(
+                                    style: TextStyle(
+                                        fontFamily: "Jost", fontSize: 17),
+                                    children: [
+                                      TextSpan(
+                                        text: "Produzido com ",
+                                      ),
+                                      TextSpan(
+                                          text: "Flutter",
+                                          style: TextStyle(
+                                              color: blueScheme.primary),
+                                          children: [
+                                            WidgetSpan(
+                                              child: Icon(
+                                                Icons.open_in_new_rounded,
+                                                color: blueScheme.primary,
+                                              ),
+                                            )
+                                          ],
+                                          recognizer: TapGestureRecognizer()
+                                            ..onTap = () {
+                                              _launchUrl(_urlFlutter);
+                                            }),
+                                      TextSpan(text: " e "),
+                                      TextSpan(
+                                          text: "Material You",
+                                          style: TextStyle(
+                                              color: blueScheme.primary),
+                                          children: [
+                                            WidgetSpan(
+                                              child: Icon(
+                                                Icons.open_in_new_rounded,
+                                                color: blueScheme.primary,
+                                              ),
+                                            )
+                                          ],
+                                          recognizer: TapGestureRecognizer()
+                                            ..onTap = () {
+                                              _launchUrl(_urlMaterialYou);
+                                            })
+                                    ]),
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          });
+                    });
+                  },
+                  child: const Text("Sobre o projeto",
+                      style: TextStyle(fontFamily: "Jost", fontSize: 17)),
+                )
+              ],
+            )
+          ],
           backgroundColor: blueScheme.primary,
         ),
+        SliverToBoxAdapter(
+          child: Card(
+            surfaceTintColor: blueScheme.onBackground,
+            margin: EdgeInsets.all(20),
+            child: Text("aiai"),
+          ),
+        )
       ]),
     );
+  }
+}
+
+Future<void> _launchUrl(url) async {
+  if (!await launchUrl(url)) {
+    throw Exception('Could not launch $url');
   }
 }
