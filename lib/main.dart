4d739713365f62b2a41ce8bbcf068d065ae313fe
danilom <oculosdanilo@gmail.com>
2023-03-17 20:15:06 -0300

    dfasdfds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, import_of_legacy_library_into_null_safe
 import 'dart:async';
 import 'dart:io';
 
