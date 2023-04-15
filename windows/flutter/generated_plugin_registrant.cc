7e2318fc73eb8a747b39a9141c453c66b66fd54f
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-04-15 18:05:53 -0300

    Revert "aa"
    This reverts commit f85778cd711b4c8b8373a2ad5ebb7764262498b7.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,14 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <just_audio_windows/just_audio_windows_plugin.h>
-#include <system_theme/system_theme_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   JustAudioWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("JustAudioWindowsPlugin"));
-  SystemThemePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("SystemThemePlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   just_audio_windows
-  system_theme
   url_launcher_windows
 )
 
