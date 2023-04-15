01a8435dcd6b111bccc75efaef64e3e0234874db
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-04-15 18:16:41 -0300

    Revert "Revert "aa""
    This reverts commit 7e2318fc73eb8a747b39a9141c453c66b66fd54f.

---
+++ lib/home/profile/pp_edit.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PPEdit extends StatefulWidget {
+  const PPEdit({super.key});
+
+  @override
+  State<PPEdit> createState() => _PPEditState();
+}
+
+class _PPEditState extends State<PPEdit> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
