42f5d2e76b1ccf222ced8a122b0c93680af09fd3
oculosdanilo <oculosdanilo@gmail.com>
2023-06-25 18:15:02 -0300

    06/25/2023 18:15:02

---
+++ lib/loginScreen/login/autenticar.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:firebase_database/firebase_database.dart';
+
+Future<(bool, String?)> autenticar(
+  String usernameDigitado,
+  String senhaDigitada,
+) async {
+  DatabaseReference ref = FirebaseDatabase.instance.ref(
+    "users/${usernameDigitado.toLowerCase()}",
+  );
+  final snapshot = await ref.get();
+  if (snapshot.exists) {
+    final senha = String.fromCharCodes(
+      base64Decode(snapshot.child("senha").value.toString()),
+    );
+    if (senhaDigitada == senha) {
+      return (true, null);
+    } else {
+      return (false, "Senha incorreta: usuário já existe!");
+    }
+  } else {
+    await ref.update({
+      "senha": base64Encode(utf8.encode(senhaDigitada)),
+      "bio": "(vazio)",
+      "img": false,
+    });
+    return (
+      false,
+      "Cadastrado com sucesso! Entre as credenciais novamente para entrar"
+    );
+  }
+}
