eb873fc6f01399d859b9a3fc085afff6cc962c68
oculosdanilo <oculosdanilo@gmail.com>
2024-05-06 22:49:40 -0300

    arrumei o splash

--- android/app/src/main/kotlin/com/example/gatopedia/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gatopedia/MainActivity.kt
@@ -1,6 +1,8 @@
 package io.oculosdanilo.gatopedia
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.os.Bundle
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
